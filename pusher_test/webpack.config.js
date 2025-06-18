87ac0bf9d0360a5f69e499a4ebbe42cd46fe8af2
Baastheglass <lightningblazer123@gmail.com>
2025-06-18 15:28:33 +0500

    committing so itll let me eject please god

---
+++ pusher_test/webpack.config.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+
+module.exports = {
+  resolve: {
+    fallback: {
+      "crypto": require.resolve("crypto-browserify"),
+      "url": require.resolve("url/"),
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve("util/"),
+      "buffer": require.resolve("buffer/"),
+      "process": require.resolve("process/browser"),
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ]
+};
